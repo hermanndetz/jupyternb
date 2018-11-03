d6eca3241faa2259e6616f61f55e06d758f893cf
Hermann Detz <hermann.detz@gmail.com>
2018-11-03 17:42:35 +0100

    Version bump to 0.1.2

--- lib/version.rb
+++ lib/version.rb
@@ -4,7 +4,18 @@
 # of the MIT license.  See the LICENSE file for details.
 
 module JupyterNB
-	VERSION = "0.1.1"
+	VERSION = "0.1.2"
+	# Suppport for Julia
+
+	# VERSION = "0.1.1"
+	# Detecting python3 executable3 before calling it
+
+	# VERSION = "0.1.0"
+	# Add capability to generate notebooks with different kernels
+	# Support for Ruby and Python3
+
+	# VERSION = "0.0.1"
+	# Initial commit
 end
 
 
